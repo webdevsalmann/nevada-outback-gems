53a03a4bd8d5cf802c9db835604e787f31d15a91
webdevsalmann <webdevsalman7869@gmail.com>
2024-03-05 22:26:14 +0530

    third commit

--- src/app/(pages)/CategoryItems.jsx
+++ src/app/(pages)/CategoryItems.jsx
@@ -7,14 +7,14 @@ export default function CategoryItems() {
     return (
         <section>
             <h2>Our Well Made Crafted Jewellery</h2>
-            <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-6 flex-wrap justify-center">
                 <CategoryCard imgUrl="/images/store/rings/1.jpg" category="Rings" categoryId="j1"/>
                 <CategoryCard imgUrl="/images/store/bracelets/1.jpg" category="Bracelets" categoryId="j2"/>
                 <CategoryCard imgUrl="/images/store/earrings/1.jpg" category="Earrings" categoryId="j3"/>
                 <CategoryCard imgUrl="/images/store/pendants/1.jpg" category="Pendants" categoryId="j4"/>
                 <CategoryCard imgUrl="/images/store/others/tanzanite/tanz_ring_001g.jpg" category="Tanzanite" categoryId="t5"/>
             </div>
-            <div className="w-full flex-center">
+            <div className="mt-6 w-full flex-center">
                 <Link href='/store' className={buttonVariants({ variant: "outline" })}>View More</Link>
             </div>
         </section>
--- src/components/ui/CategoryCard.jsx
+++ src/components/ui/CategoryCard.jsx
@@ -7,7 +7,7 @@ export default function CategoryCard({ imgUrl, category, categoryId }) {
     const {setdisplayItems}= useData();
     return (
         // <div className={`relative min-w-60 aspect-[12/18] bg-[url('${imgUrl ? imgUrl : "/images/store/bracelets/1.jpg"}')] bg-center bg-cover bg-no-repeat rounded-lg overflow-hidden`}>
-        <Link href="/store/#store" className={`relative min-w-60 max-w-72 aspect-[12/18] rounded-lg overflow-hidden flex-1`} onClick={()=>setdisplayItems(categoryId)}>
+        <Link href="/store/#store" className={`relative min-w-60 max-w-72 aspect-[12/18] rounded-lg overflow-hidden flex-1 border`} onClick={()=>setdisplayItems(categoryId)}>
             <div className="absolute w-full h-full">
                 <Image className="w-full h-full object-cover" src={imgUrl} width={1200} height={1800} alt={category} />
             </div>
